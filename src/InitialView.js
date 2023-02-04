1537927889eca1e00873c66fc591ef471567fd2f
dd114 <40179423+dd114@users.noreply.github.com>
2023-02-04 21:30:16 +0500

    2.5.0

--- src/InitialView.js
+++ src/InitialView.js
@@ -8,16 +8,30 @@ import {
     ConfigProvider,
     SplitLayout,
     Epic, TabbarItem, Tabbar,
-    PanelHeader, Group, Header,
+    PanelHeader, Group, ScreenSpinner,
     Cell, Avatar, Progress, FormItem
 } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
-import {Icon24GameOutline, Icon28NewsfeedOutline, Icon28AddOutline} from "@vkontakte/icons";
+import {Icon24GameOutline, Icon28NewsfeedOutline, Icon28AddOutline, Icon28LikeFillRed} from "@vkontakte/icons";
+
 
 import Results from "./Results";
 
 import fireStore from "./DB";
-import {collection, doc, updateDoc, getDoc, setDoc, serverTimestamp} from 'firebase/firestore'
+import {
+    collection,
+    doc,
+    updateDoc,
+    getDoc,
+    getDocs,
+    setDoc,
+    serverTimestamp,
+    query,
+    where,
+    orderBy,
+    limit,
+    onSnapshot
+} from 'firebase/firestore'
 
 const InitialView = () => {
 
@@ -26,6 +40,8 @@ const InitialView = () => {
     const [scheme, setScheme] = useState('bright_light')
     const [fetchedUser, setUser] = useState(null);
     const [percent, setPercent] = useState(70);
+
+    const [popout, setPopout] = useState(<ScreenSpinner state="loading"/>);
 
     const onStoryChange = (e) => {
         setActivePanel(e.currentTarget.dataset.story)
@@ -47,26 +63,76 @@ const InitialView = () => {
         fetchData();
     }, []);
 
+    const reciveData = async (q) => {
+
+        const inf = []
+
+        const querySnapshot = await getDocs(q)
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+            inf.push(doc)
+        })
+
+        // console.log(inf)
+        return inf
+    }
+
+    const sendData = (inf) => {
+        setDoc(doc(fireStore, 'users', inf.idVK.toString()), inf).then(docRef => {
+            console.log("Information has been added successfully", docRef);
+        }).catch(error => {
+            // console.error(error)
+            throw new Error("Whoops! " + error)
+        })
+    }
+
+    let lastScore = null, bestScore = null, lastEnterTime = null, currentScore = null
+    let userInfo = null
+
     useEffect(() => {
         bridge.send('VKWebAppGetUserInfo')
             .then((data) => {
-                console.log(data)
+                userInfo = data
 
-                setDoc(doc(fireStore, 'users', data.id.toString()), {
-                    bestScore: 0, Name: data.first_name, Surname: data.last_name,
-                    lastEnterTime: serverTimestamp(),
-                    idVK: data.id
-                }).then(docRef => {
-                    console.log("Document has been added successfully");
+                // RECIVE AND SEND
+                const q = query(collection(fireStore, "users"), where("idVK", "!=", userInfo.id))
+                // const q = query(collection(fireStore, "users"), where("bestScore", "==", 0))
+                reciveData(q).then(inf => {
+                    if (inf.length === 1) {
+                        lastScore = inf[0].data().lastScore
+                        bestScore = inf[0].data().bestScore
+                        lastEnterTime = inf[0].data().lastEnterTime
+
+                        // console.log("From Db inf", lastScore, bestScore, lastEnterTime)
+                    } else {
+                        currentScore = 0
+
+                        sendData({
+                            bestScore: 0, Name: userInfo.first_name, Surname: userInfo.last_name,
+                            // lastEnterTime: serverTimestamp(),
+                            lastEnterTime: Date.now(),
+                            idVK: userInfo.id, lastScore: currentScore
+                        })
+
+                        console.error(`inf.length = ${inf.length}`)
+
+                    }
+
                 }).catch(error => {
-                    console.log(error);
-                });
-            })
-            .catch((error) => {
-                // Ошибка
-                console.log(error);
-            })
-    }, []);
+                    throw new Error("Whoops! " + error)
+                })
+
+
+            }).catch((error) => {
+            throw new Error("Whoops! " + error)
+        })
+
+
+        setPopout(null)
+
+
+    }, [])
 
 
     const lessOrEqual100 = (v, add) => {
@@ -78,9 +144,9 @@ const InitialView = () => {
         <ConfigProvider appearance={scheme}>
             <AdaptivityProvider>
                 <AppRoot>
-                    <SplitLayout>
+                    <SplitLayout popout={popout}>
 
-                        <Epic
+                        {!popout && <Epic
                             activeStory={activePanel}
                             tabbar={
 
@@ -112,88 +178,84 @@ const InitialView = () => {
                                 <Panel id={'game'}>
                                     <PanelHeader>Tamagotchi</PanelHeader>
 
-                                    {fetchedUser &&
-                                        <Group mode="card">
-                                            <Cell
-                                                before={fetchedUser.photo_200 ?
-                                                    <Avatar src={fetchedUser.photo_200}/> : null}
-                                                description={"У вас сейчас 300 очков!"}
-                                            >
-                                                {`${fetchedUser.first_name} ${fetchedUser.last_name}`}
-                                            </Cell>
-                                        </Group>
-                                    }
 
-                                    {fetchedUser &&
-                                        <Group mode="plain">
-                                            <Cell
-                                                // before={fetchedUser.photo_200 ?
-                                                //     <Avatar src={fetchedUser.photo_200}/> : null}
-                                                description={"Нужно упорно репетировать..."}
-                                                after={<Icon28AddOutline/>}
-                                                onClick={() => setPercent(lessOrEqual100(percent, 2))}
-                                            >
-                                                Репетиция в авангарде
-                                            </Cell>
+                                    <Group mode="card">
+                                        <Cell
+                                            before={<Avatar
+                                                src={"https://sun3-12.userapi.com/s/v1/ig2/nOpTni7bX_hgwLHb0Nl_u_HDE7-ezKWlOM8LGjll8ccb448jif_WMKkImvMGSVmsUopV3SEr_ovkh2n4plhKI0AP.jpg?size=200x200&quality=95&crop=325,125,1073,1073&ava=1"}/>}
+                                            description={"У вас и вашего мистера сейчас 300 очков!️ 💋"}
+                                            // after={<Icon28LikeFillRed/>}
+                                        >
+                                            Ваш личный Марсель Нуретдинов 💚
+                                        </Cell>
+                                    </Group>
 
-                                            <FormItem id="progresslabel" top={`Прогресс: ${percent}%`}>
-                                                <Progress aria-labelledby="progresslabel" value={percent}/>
-                                            </FormItem>
-                                        </Group>
-                                    }
 
-                                    {fetchedUser &&
-                                        <Group mode="plain">
-                                            <Cell
-                                                // before={fetchedUser.photo_200 ?
-                                                //     <Avatar src={fetchedUser.photo_200}/> : null}
-                                                description={"А он кста еще и духом сильный!"}
-                                                after={<Icon28AddOutline/>}
-                                                onClick={() => localStorage.setItem('myCat', 'Tom')}
-                                            >
-                                                Качалочка
-                                            </Cell>
+                                    <Group mode="plain">
+                                        <Cell
+                                            // before={fetchedUser.photo_200 ?
+                                            //     <Avatar src={fetchedUser.photo_200}/> : null}
+                                            description={"Нужно упорно репетировать..."}
+                                            after={<Icon28AddOutline/>}
+                                            onClick={() => setPercent(lessOrEqual100(percent, 2))}
+                                        >
+                                            Репетиция в авангарде
+                                        </Cell>
 
-                                            <FormItem id="progresslabel" top="Прогресс: 40%">
-                                                <Progress aria-labelledby="progresslabel" value={40}/>
-                                            </FormItem>
-                                        </Group>
-                                    }
+                                        <FormItem id="progresslabel" top={`Прогресс: ${percent}%`}>
+                                            <Progress aria-labelledby="progresslabel" value={percent}/>
+                                        </FormItem>
+                                    </Group>
 
-                                    {fetchedUser &&
-                                        <Group mode="plain">
-                                            <Cell
-                                                // before={fetchedUser.photo_200 ?
-                                                //     <Avatar src={fetchedUser.photo_200}/> : null}
-                                                description={"Матеша, блинб"}
-                                                after={<Icon28AddOutline/>}
-                                                onClick={() => console.log(localStorage.getItem('myCat'))}
-                                            >
-                                                Учеба
-                                            </Cell>
 
-                                            <FormItem id="progresslabel" top="Прогресс: 40%">
-                                                <Progress aria-labelledby="progresslabel" value={40}/>
-                                            </FormItem>
-                                        </Group>
-                                    }
+                                    <Group mode="plain">
+                                        <Cell
+                                            // before={fetchedUser.photo_200 ?
+                                            //     <Avatar src={fetchedUser.photo_200}/> : null}
+                                            description={"А он еще и духом сильный!"}
+                                            after={<Icon28AddOutline/>}
+                                            onClick={() => localStorage.setItem('myCat', 'Tom')}
+                                        >
+                                            Качалочка
+                                        </Cell>
 
-                                    {fetchedUser &&
-                                        <Group mode="plain">
-                                            <Cell
-                                                // before={fetchedUser.photo_200 ?
-                                                //     <Avatar src={fetchedUser.photo_200}/> : null}
-                                                description={"Роберт Фаридович будет доволен"}
-                                                after={<Icon28AddOutline/>}
-                                            >
-                                                Участие в мероприятиях
-                                            </Cell>
+                                        <FormItem id="progresslabel" top="Прогресс: 40%">
+                                            <Progress aria-labelledby="progresslabel" value={40}/>
+                                        </FormItem>
+                                    </Group>
 
-                                            <FormItem id="progresslabel" top="Прогресс: 40%">
-                                                <Progress aria-labelledby="progresslabel" value={40}/>
-                                            </FormItem>
-                                        </Group>
-                                    }
+
+                                    <Group mode="plain">
+                                        <Cell
+                                            // before={fetchedUser.photo_200 ?
+                                            //     <Avatar src={fetchedUser.photo_200}/> : null}
+                                            description={"Блин, нужно же еще и диплом писать!"}
+                                            after={<Icon28AddOutline/>}
+                                            onClick={() => console.log(localStorage.getItem('myCat'))}
+                                        >
+                                            Учеба
+                                        </Cell>
+
+                                        <FormItem id="progresslabel" top="Прогресс: 40%">
+                                            <Progress aria-labelledby="progresslabel" value={40}/>
+                                        </FormItem>
+                                    </Group>
+
+
+                                    <Group mode="plain">
+                                        <Cell
+                                            // before={fetchedUser.photo_200 ?
+                                            //     <Avatar src={fetchedUser.photo_200}/> : null}
+                                            description={"Роберт Фаридович будет доволен"}
+                                            after={<Icon28AddOutline/>}
+                                        >
+                                            Участие в мероприятиях
+                                        </Cell>
+
+                                        <FormItem id="progresslabel" top="Прогресс: 40%">
+                                            <Progress aria-labelledby="progresslabel" value={40}/>
+                                        </FormItem>
+                                    </Group>
 
 
                                 </Panel>
@@ -206,7 +268,7 @@ const InitialView = () => {
                                 </Panel>
                             </View>
 
-                        </Epic>
+                        </Epic>}
 
 
                     </SplitLayout>
