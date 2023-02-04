ebd52a33cccd0175149e8e3861a4fb2ba4524ce8
dd114 <40179423+dd114@users.noreply.github.com>
2023-02-04 21:31:58 +0500

    2.5.1 - forgotten files have been added

---
+++ src/DB.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDPsZ3P2cEuVRKtso3z4h3EF0G0ninM-yY",
+    authDomain: "mr-usatu.firebaseapp.com",
+    projectId: "mr-usatu",
+    storageBucket: "mr-usatu.appspot.com",
+    messagingSenderId: "729460079370",
+    appId: "1:729460079370:web:787d2ed427f96bcd22d8f3",
+    measurementId: "G-PHWBER8R7V"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const fireStore = getFirestore(app)
+
+
+
+export default fireStore;
---
+++ src/Results.js
@@ -0,0 +1,104 @@
+import React, {useEffect, useState} from 'react';
+import bridge from '@vkontakte/vk-bridge';
+import {
+    Cell, Avatar, Group, PanelHeader
+} from '@vkontakte/vkui';
+import '@vkontakte/vkui/dist/vkui.css';
+import {
+    collection,
+    doc,
+    getDocs,
+    limit,
+    orderBy,
+    query,
+    serverTimestamp,
+    setDoc,
+    where,
+    onSnapshot
+} from "firebase/firestore";
+import fireStore from "./DB";
+
+
+const Results = () => {
+
+    const [fullInformation, setFullInformation] = useState([])
+
+    useEffect(() => {
+
+        const tempArr = []
+        // const ref = query(collection(fireStore, 'users'))
+        // const q = query(collection(fireStore, "users"), where("bestScore", "!=", -1));
+        const q = query(collection(fireStore, "users"), orderBy('bestScore', 'desc'), limit(50));
+
+        // console.log("ref", ref)
+        console.log("q", q)
+
+        let topId = []
+        let topBestScore = []
+
+        onSnapshot(q, (snap) => {
+            console.log("snap", snap)
+            console.log("snap.docs", snap.docs)
+            snap.docs.forEach(element => {
+                let idVK = element._document.data.value.mapValue.fields.idVK.integerValue
+                let bestScore = element._document.data.value.mapValue.fields.bestScore.integerValue
+                console.log("element", idVK)
+                topId.push(idVK)
+                topBestScore.push(bestScore)
+            })
+
+            // console.log("top", topId)
+
+
+            bridge.send("VKWebAppCallAPIMethod", {
+                "method": "users.get",
+                "request_id": "32test",
+                "params": {
+                    "user_ids": topId.join(','),
+                    "v": "5.131",
+                    "fields": "photo_100",
+                    "access_token": "43d622264f5270475b941c24bc117d252490fe0d97ef581ec7b44321cb1f3df7aa970bf00bc50d4e641db"
+                }
+            }).then((data) => {
+                // console.log("data", data)
+
+                data.response.map((el, i) => {
+                    tempArr.push({
+                        idVK: el.id, first_name: el.first_name,
+                        last_name: el.last_name, ava: el.photo_100, bestScore: topBestScore[i]
+                    })
+                })
+
+                console.log("tempArr", tempArr)
+                console.log("topBestScore", topBestScore)
+
+                setFullInformation(tempArr)
+                // fullInformation.length = 0
+            }).catch((error) => {
+                // Ошибка
+                console.error(error);
+            })
+
+
+        })
+
+
+    }, [])
+
+    return (
+        <React.Fragment>
+            <PanelHeader>Best players</PanelHeader>
+            <Group>
+                {fullInformation.map(
+                    (v, i) => {
+                        console.log("key=", i)
+                        return <Cell key={i} before={<Avatar src={v.ava}></Avatar>}
+                                     after={'Total: ' + v.bestScore}>{v.first_name + ' ' + v.last_name}</Cell>
+                    }
+                )}
+            </Group>
+        </React.Fragment>
+    )
+}
+
+export default Results
